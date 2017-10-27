ba84ed2789bbbf9f94ca564b14ef02835582c79e
Corey Johnson <cor.johnson@me.com>
2017-10-26 18:54:19 -0700

    Trying to add new types for media items

--- MediaTracker/app/src/main/java/co/miniforge/corey/mediatracker/model/MediaItem.java
+++ MediaTracker/app/src/main/java/co/miniforge/corey/mediatracker/model/MediaItem.java
@@ -20,6 +20,10 @@ public class MediaItem {
     public String description;
     public String url;
 
+    //We need a field that stores what kind of media item this is
+    //We will do this by creating an enum that can identify the type of item
+    public MediaItemType type = MediaItemType.Generic;
+
     public MediaItem(JSONObject jsonObject){
         try{
             //Generate id based on the object instance (should work :D)
@@ -27,6 +31,8 @@ public class MediaItem {
             this.title = jsonObject.getString("title");
             this.description = jsonObject.getString("description");
             this.url = jsonObject.getString("url");
+
+            this.type = getTypeForObject((MediaItemType)jsonObject.get("type"));
         } catch (Exception e){
             Log.e("toJSONError", String.format("There was an error: %s", e.getMessage()));
         }
@@ -39,6 +45,15 @@ public class MediaItem {
         this.url = "defaultUrl";
     }
 
+    public MediaItemType getTypeForObject(MediaItemType value){
+        switch (value){
+            case Generic:
+                break;
+        }
+
+        return MediaItemType.Generic;
+    }
+
     public JSONObject toJson(){
         JSONObject mediaItem = new JSONObject();
 
@@ -47,6 +62,8 @@ public class MediaItem {
             mediaItem.put("title", this.title);
             mediaItem.put("description", this.description);
             mediaItem.put("url", this.url);
+
+            mediaItem.put("type", this.type);
         } catch (Exception e){
             Log.e("toJSONError", String.format("There was an error: %s", e.getMessage()));
         }
--- MediaTracker/app/src/main/java/co/miniforge/corey/mediatracker/model/TVItem.java
+++ MediaTracker/app/src/main/java/co/miniforge/corey/mediatracker/model/TVItem.java
@@ -7,7 +7,18 @@ import org.json.JSONObject;
  */
 
 public class TVItem extends MediaItem {
+    //Fields
+    public int currentEpisodesWatched;
+    public int totalEpisodes;
+
     public TVItem(JSONObject jsonObject) {
         super(jsonObject);
+
+        try {
+            currentEpisodesWatched = jsonObject.getInt("currentEpisodesWatched");
+            totalEpisodes = jsonObject.getInt("totalEpisodes");
+        } catch (Exception e) {
+            //TODO: Create exception logger
+        }
     }
 }
