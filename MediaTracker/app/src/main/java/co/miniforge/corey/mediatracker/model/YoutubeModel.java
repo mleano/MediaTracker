4ec05397acb6b95d91f1d3bdf399bc97acb9cc98
Marlene Leano <mleano@mail.greenriver.edu>
2017-11-18 01:44:43 -0800

    updated movie detail layout

--- MediaTracker/app/src/main/java/co/miniforge/corey/mediatracker/model/YoutubeModel.java
+++ MediaTracker/app/src/main/java/co/miniforge/corey/mediatracker/model/YoutubeModel.java
@@ -1,8 +1,11 @@
 package co.miniforge.corey.mediatracker.model;
 
 /**
- * Created by marle on 11/18/2017.
+ * Created by Marlene on 11/18/2017.
  */
 
 public class YoutubeModel extends MediaItem {
+    //Fields
+    public String youtuberName;
+    public int duration;
 }
