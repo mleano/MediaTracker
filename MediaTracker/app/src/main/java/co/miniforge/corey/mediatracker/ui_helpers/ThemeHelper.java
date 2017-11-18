ed99202df49bf73b15dd82c101e22383aaebec5d
Marlene Leano <mleano@mail.greenriver.edu>
2017-11-18 00:44:48 -0800

    added fields and the constructor

--- MediaTracker/app/src/main/java/co/miniforge/corey/mediatracker/ui_helpers/ThemeHelper.java
+++ MediaTracker/app/src/main/java/co/miniforge/corey/mediatracker/ui_helpers/ThemeHelper.java
@@ -1,8 +1,23 @@
 package co.miniforge.corey.mediatracker.ui_helpers;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+
 /**
  * Created by Marlene on 11/18/2017.
  */
 
 public class ThemeHelper {
+    //Fields
+    private SharedPreferences sharedPref;
+    private int themeLightBg;
+    private int themeDarkBg;
+
+
+    //Constructors
+    public ThemeHelper(Context context){}
+
+
+    //Methods
+
 }
