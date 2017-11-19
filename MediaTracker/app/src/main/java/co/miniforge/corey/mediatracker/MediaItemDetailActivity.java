c95a3301c121137a783afd6b17277b797b3ef46e
Marlene Leano <mleano@mail.greenriver.edu>
2017-11-19 00:03:32 -0800

    created function to prompt for a confirmation dialog before saving

--- MediaTracker/app/src/main/java/co/miniforge/corey/mediatracker/MediaItemDetailActivity.java
+++ MediaTracker/app/src/main/java/co/miniforge/corey/mediatracker/MediaItemDetailActivity.java
@@ -1,6 +1,8 @@
 package co.miniforge.corey.mediatracker;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -95,6 +97,9 @@ public class MediaItemDetailActivity extends AppCompatActivity {
         saveBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                //Call function to prompt a confirmation dialog before saving
+                promptConfirmation();
+
                 //Create intent that would direct back to MyListActivity
                 Intent intent = new Intent(getApplicationContext(), MyListActivity.class);
 
@@ -111,5 +116,31 @@ public class MediaItemDetailActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+    //Function that prompts for a confirmation dialog
+    private void promptConfirmation() {
+
+        //https://developer.android.com/guide/topics/ui/dialogs.html#AlertDialog
+        //Make sure to put the code in the activity, the builder requires an activity to be passed in
+        //import android.support.v7.app for the alert dialog
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Save Changes").setMessage("Are you sure you want to save these changes?");
+
+        builder.setPositiveButton("Save", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                // Put the start activity with intent code here
+            }
+        });
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                // Do nothing, unless you want this button to go back to
+                // ListActivity without putting an intent extra
+            }
+        });
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
     }
 }
