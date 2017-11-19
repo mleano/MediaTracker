5170299685bd6d4ebb28ec9373b51ec78eacf5dc
Marlene Leano <mleano@mail.greenriver.edu>
2017-11-18 23:04:51 -0800

    added a constructor

--- MediaTracker/app/src/main/java/co/miniforge/corey/mediatracker/model/YoutubeModel.java
+++ MediaTracker/app/src/main/java/co/miniforge/corey/mediatracker/model/YoutubeModel.java
@@ -1,5 +1,7 @@
 package co.miniforge.corey.mediatracker.model;
 
+import org.json.JSONObject;
+
 /**
  * Created by Marlene on 11/18/2017.
  */
@@ -8,4 +10,9 @@ public class YoutubeModel extends MediaItem {
     //Fields
     public String youtuberName;
     public int duration;
+
+    //Constructor
+    public YoutubeModel(JSONObject jsonObject) {
+        super(jsonObject);
+    }
 }
