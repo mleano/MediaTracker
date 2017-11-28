d4c02355dba21d036072e6c8a919e9d25c0cf18c
Marlene Leano <mleano@mail.greenriver.edu>
2017-11-27 22:44:57 -0800

    Revert "added fields and the constructor"
    This reverts commit ed99202df49bf73b15dd82c101e22383aaebec5d.

--- MediaTracker/app/src/main/java/co/miniforge/corey/mediatracker/ui_helpers/ThemeHelper.java
+++ MediaTracker/app/src/main/java/co/miniforge/corey/mediatracker/ui_helpers/ThemeHelper.java
@@ -1,23 +1,8 @@
 package co.miniforge.corey.mediatracker.ui_helpers;
 
-import android.content.Context;
-import android.content.SharedPreferences;
-
 /**
  * Created by Marlene on 11/18/2017.
  */
 
 public class ThemeHelper {
-    //Fields
-    private SharedPreferences sharedPref;
-    private int themeLightBg;
-    private int themeDarkBg;
-
-
-    //Constructors
-    public ThemeHelper(Context context){}
-
-
-    //Methods
-
 }
