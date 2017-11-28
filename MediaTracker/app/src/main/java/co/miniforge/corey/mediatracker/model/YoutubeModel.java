0d2888652c3a486774de8a3953b8b5103fd24751
Marlene Leano <mleano@mail.greenriver.edu>
2017-11-27 22:44:42 -0800

    Revert "updated movie detail layout"
    This reverts commit 4ec05397acb6b95d91f1d3bdf399bc97acb9cc98.

--- MediaTracker/app/src/main/java/co/miniforge/corey/mediatracker/model/YoutubeModel.java
+++ MediaTracker/app/src/main/java/co/miniforge/corey/mediatracker/model/YoutubeModel.java
@@ -1,11 +1,8 @@
 package co.miniforge.corey.mediatracker.model;
 
 /**
- * Created by Marlene on 11/18/2017.
+ * Created by marle on 11/18/2017.
  */
 
 public class YoutubeModel extends MediaItem {
-    //Fields
-    public String youtuberName;
-    public int duration;
 }
