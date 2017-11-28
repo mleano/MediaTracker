6fb147be2faf10283617fe3c1c8cdba42758d876
Marlene Leano <mleano@mail.greenriver.edu>
2017-11-27 22:44:45 -0800

    Revert "updated onOptionsItemSelected function"
    This reverts commit 991dacd4479d0b35b54c249abb5bcd2d981ec2ae.

--- MediaTracker/app/src/main/java/co/miniforge/corey/mediatracker/SettingsActivity.java
+++ MediaTracker/app/src/main/java/co/miniforge/corey/mediatracker/SettingsActivity.java
@@ -2,33 +2,12 @@ package co.miniforge.corey.mediatracker;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-
-import co.miniforge.corey.mediatracker.ui_helpers.ThemeHelper;
 
 public class SettingsActivity extends AppCompatActivity {
-    //Fields
-    ThemeHelper themeHelper;
-    Button toggleBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
-
-        //Initialize ThemeHelper
-        themeHelper = new ThemeHelper(getApplicationContext());
-
-        //Store a reference to the toggle theme button
-        toggleBtn = (Button) findViewById(R.id.theme_toggle_btn);
-
-        //When the toggle button is clicked, change the theme
-        toggleBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                themeHelper.enableDarkTheme(!themeHelper.darkThemeEnabled());
-            }
-        });
     }
 }
