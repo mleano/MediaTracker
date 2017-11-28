bc32351a13b273d38abe63757f07acf83c7a22b3
Marlene Leano <mleano@mail.greenriver.edu>
2017-11-27 22:44:54 -0800

    Revert "add more fields for the other colors and defined them in the try block"
    This reverts commit e7d717fe3af91dd64c964513e6255361a781421e.

--- MediaTracker/app/src/main/java/co/miniforge/corey/mediatracker/ui_helpers/ThemeHelper.java
+++ MediaTracker/app/src/main/java/co/miniforge/corey/mediatracker/ui_helpers/ThemeHelper.java
@@ -2,11 +2,8 @@ package co.miniforge.corey.mediatracker.ui_helpers;
 
 import android.content.Context;
 import android.content.SharedPreferences;
-import android.support.v4.content.ContextCompat;
 import android.view.View;
 import android.widget.TextView;
-
-import co.miniforge.corey.mediatracker.R;
 
 /**
  * Created by Marlene on 11/18/2017.
@@ -16,26 +13,11 @@ public class ThemeHelper {
     //Fields
     private SharedPreferences sharedPref;
     private int themeLightBg;
-    private int themeLightContainer;
-    private int themeLightText;
     private int themeDarkBg;
-    private int themeDarkContainer;
-    private int themeDarkText;
 
 
     //Constructors
-    public ThemeHelper(Context context){
-        try{
-            themeLightBg = ContextCompat.getColor(context, R.color.themeLightBg);
-            themeLightContainer = ContextCompat.getColor(context, R.color.themeLightContainer);
-            themeLightText = ContextCompat.getColor(context, R.color.themeLightText);
-            themeDarkBg = ContextCompat.getColor(context, R.color.themeDarkBg);
-            themeDarkContainer = ContextCompat.getColor(context, R.color.themeDarkContainer);
-            themeDarkText = ContextCompat.getColor(context, R.color.themeDarkText);
-        } catch (Exception e) {
-
-        }
-    }
+    public ThemeHelper(Context context){}
 
 
     //Methods
