9e72e9d6c643515785e27189ab3df09a93029804
Marlene Leano <mleano@mail.greenriver.edu>
2017-11-27 22:44:35 -0800

    Revert "added a case for a youtube type in the switch"
    This reverts commit ceb226e1839426dd136d77e28be3b3b416b75cea.

--- MediaTracker/app/src/main/java/co/miniforge/corey/mediatracker/model/MediaItem.java
+++ MediaTracker/app/src/main/java/co/miniforge/corey/mediatracker/model/MediaItem.java
@@ -60,8 +60,6 @@ public class MediaItem {
                 return  MediaItemType.TV;
             case "Movie":
                 return  MediaItemType.Movie;
-            case "YouTube":
-                return MediaItemType.YouTube;
             default:
                 return MediaItemType.Generic;
         }
