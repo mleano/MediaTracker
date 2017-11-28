7a90b5cd86f34157badcb435baecdc62b2f63509
Marlene Leano <mleano@mail.greenriver.edu>
2017-11-27 22:44:55 -0800

    Revert "added headers for the methods"
    This reverts commit 191a99d9671a397d7909cdb533c39f6a01e413e8.

--- MediaTracker/app/src/main/java/co/miniforge/corey/mediatracker/ui_helpers/ThemeHelper.java
+++ MediaTracker/app/src/main/java/co/miniforge/corey/mediatracker/ui_helpers/ThemeHelper.java
@@ -2,8 +2,6 @@ package co.miniforge.corey.mediatracker.ui_helpers;
 
 import android.content.Context;
 import android.content.SharedPreferences;
-import android.view.View;
-import android.widget.TextView;
 
 /**
  * Created by Marlene on 11/18/2017.
@@ -21,17 +19,5 @@ public class ThemeHelper {
 
 
     //Methods
-    public boolean darkThemeEnabled() {
-        return false;
-    }
-
-    public void enableDarkTheme(boolean enabled) {}
-
-    public void themeTextView(TextView...textviews) {}
-
-    public void themeImageContainer(View...containers) {}
-
-    public void themeBackground(View rootview) {}
-
 
 }
