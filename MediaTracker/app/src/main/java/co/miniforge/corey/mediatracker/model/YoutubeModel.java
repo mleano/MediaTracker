714f06846ac88835eddc694debe2a534d4eaefdb
Marlene Leano <mleano@mail.greenriver.edu>
2017-11-27 22:44:36 -0800

    Revert "added a constructor"
    This reverts commit 5170299685bd6d4ebb28ec9373b51ec78eacf5dc.

--- MediaTracker/app/src/main/java/co/miniforge/corey/mediatracker/model/YoutubeModel.java
+++ MediaTracker/app/src/main/java/co/miniforge/corey/mediatracker/model/YoutubeModel.java
@@ -1,7 +1,5 @@
 package co.miniforge.corey.mediatracker.model;
 
-import org.json.JSONObject;
-
 /**
  * Created by Marlene on 11/18/2017.
  */
@@ -10,9 +8,4 @@ public class YoutubeModel extends MediaItem {
     //Fields
     public String youtuberName;
     public int duration;
-
-    //Constructor
-    public YoutubeModel(JSONObject jsonObject) {
-        super(jsonObject);
-    }
 }
